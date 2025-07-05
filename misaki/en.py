828c0b816994f085007ba452cfabded2d9659fe7
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-06 06:45:41 +0700

    Postprocess vi g2p and en g2p to unified phoneme set

--- misaki/en.py
+++ misaki/en.py
@@ -672,7 +672,7 @@ class G2P:
 
     def _post_process(self, raw_phonemes):
         """Applies the UNIFICATION_MAP to a raw phoneme string."""
-        return UNIFICATION_REGEX.sub(lambda m: self.UNIFICATION_MAP[m.group(0)], raw_phonemes)
+        return UNIFICATION_REGEX.sub(lambda m: UNIFICATION_MAP[m.group(0)], raw_phonemes)
 
     def __call__(self, text: str, preprocess=True) -> Tuple[str, List[MToken]]:
         preprocess = G2P.preprocess if preprocess == True else preprocess
